6687e8e49fb26fdacba0ed7e666d1866514b2d5e
sikamloïque <loicsikam272@gmail.com>
2022-09-10 00:58:50 +0200

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  SERVER_URL: 'http://localhost:4000',
+  SERVER_URL: 'https://bulletinapi.herokuapp.com',
   production: false
 };
 
